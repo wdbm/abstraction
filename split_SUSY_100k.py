030e6f0cc60922ad2a40b9e4a5e1dfeeeb2210ee
Will Breaden Madden <wbm@protonmail.ch>
2017-03-02 22:57:52 +0000

    add maximum events arguments to split_SUSY_100k.py, add SUSY_100k_signal.csv, add SUSY_100k_background.csv

--- split_SUSY_100k.py
+++ split_SUSY_100k.py
@@ -33,21 +33,25 @@ usage:
     program [options]
 
 options:
-    -h, --help               display help message
-    --version                display version and exit
-    -v, --verbose            verbose logging
-    -s, --silent             silent
-    -u, --username=USERNAME  username
-    --data=FILENAME          input data file
-                             [default: SUSY_100k.csv]
-    --signal=FILENAME        output signal data file
-                             [default: SUSY_100k_signal.csv]
-    --background=FILENAME    output background data file
-                             [default: SUSY_100k_background.csv]
+    -h, --help                        display help message
+    --version                         display version and exit
+    -v, --verbose                     verbose logging
+    -s, --silent                      silent
+    -u, --username=USERNAME           username
+    --data=FILENAME                   input data file
+                                      [default: SUSY_100k.csv]
+    --signal=FILENAME                 output signal data file
+                                      [default: SUSY_100k_signal.csv]
+    --background=FILENAME             output background data file
+                                      [default: SUSY_100k_background.csv]
+    --maximumeventssignal=NUMBER      maximum number of signal events saved
+                                      [default: none]
+    --maximumeventsbackground=NUMBER  maximum number of signal events saved
+                                      [default: none]
 """
 
 name    = "split_SUSY_100k"
-version = "2016-12-14T1841Z"
+version = "2017-03-02T2256Z"
 logo    = None
 
 import csv
@@ -62,6 +66,17 @@ def main(options):
     filename_input_data             = options["--data"]
     filename_output_data_signal     = options["--signal"]
     filename_output_data_background = options["--background"]
+    maximum_events_signal           = options["--maximumeventssignal"]
+    maximum_events_background       = options["--maximumeventsbackground"]
+    if options["--maximumeventssignal"].lower() == "none":
+        maximum_events_signal = None
+    else:
+        maximum_events_signal = int(options["--maximumeventssignal"])
+
+    if options["--maximumeventsbackground"].lower() == "none":
+        maximum_events_background = None
+    else:
+        maximum_events_background = int(options["--maximumeventsbackground"])
 
     print("input data file: {filename}".format(
         filename = filename_input_data
@@ -70,13 +85,37 @@ def main(options):
     data = [line.rstrip("\n") for line in open(filename_input_data)]
     data_signal     = []
     data_background = []
+
     for line in data:
         line_list = line.split(",")
-        classification_event = float(line_list[0])
+        classification_event = float(line_list[0])            
         if classification_event == 1:
-            data_signal.append(line_list[1:])
+            if maximum_events_signal is None:
+                data_signal.append(line_list[1:])
+            elif len(data_signal) < maximum_events_signal:
+                data_signal.append(line_list[1:])
+
+    for line in data:
+        line_list = line.split(",")
+        classification_event = float(line_list[0])            
         if classification_event == 0:
-            data_background.append(line_list[1:])
+            if maximum_events_background is None:
+                data_background.append(line_list[1:])
+            elif len(data_background) < maximum_events_background:
+                data_background.append(line_list[1:])
+
+    print("")
+    print("number of lines in input data:             {number_of_lines}".format(
+        number_of_lines = len(data)
+    ))
+    print("number of lines in output signal data:     {number_of_lines}".format(
+        number_of_lines = len(data_signal)
+    ))
+    print("number of lines in output background data: {number_of_lines}".format(
+        number_of_lines = len(data_background)
+    ))
+
+    print("")
     print("save signal data to file {filename}".format(
         filename = filename_output_data_signal
     ))
