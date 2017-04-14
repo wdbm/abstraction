893b5fcd50348b5a9c2bae79c7e484af2023dc42
Will Breaden Madden <wbm@protonmail.ch>
2017-04-14 20:50:00 +0100

    fix update of run_archive.sh

--- run_archive.sh
+++ run_archive.sh
@@ -5,3 +5,6 @@ version="2017-04-14T1845Z"
 python arcodex.py --numberOfUtterances 200 --subreddits=askreddit,changemyview,lgbt,machinelearning,particlephysics,technology,worldnews --verbose #--database=""$(date "+%Y-%m-%dT%H%MZ" --utc)".db"
 
 python abstraction_save_tweets_of_top_followed_users_Twitter_to_database.py
+duplicates_database_SQLite.py --databasein=Twitter.db --databaseout=Twitter_1.db
+rm Twitter.db
+mv Twitter_1.db Twitter.db
