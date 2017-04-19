edc8800b5aff83751be76a320b7a11d13bd4d409
Will Breaden Madden <wbm@protonmail.ch>
2017-04-19 12:06:19 +0100

    add backups to run archive script, update Twitter database

--- run_archive.sh
+++ run_archive.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-version="2017-04-14T1845Z"
+version="2017-04-19T1049Z"
 
 python arcodex.py --numberOfUtterances 200 --subreddits=askreddit,changemyview,lgbt,machinelearning,particlephysics,technology,worldnews --verbose #--database=""$(date "+%Y-%m-%dT%H%MZ" --utc)".db"
 
+cp Twitter.db ""$(date "+%Y-%m-%dT%H%MZ" --utc)"_backup_Twitter.db"
 python abstraction_save_tweets_of_top_followed_users_Twitter_to_database.py
-duplicates_database_SQLite.py --databasein=Twitter.db --databaseout=Twitter_1.db
-rm Twitter.db
-mv Twitter_1.db Twitter.db
+mv Twitter.db Twitter_0.db
+duplicates_database_SQLite.py --databasein=Twitter_0.db --databaseout=Twitter.db
+#rm Twitter_0.db
