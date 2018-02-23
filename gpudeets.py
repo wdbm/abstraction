2b65efc4c3e812e47ceff0698bdb78884769857c
Will Breaden Madden <wbm@protonmail.ch>
2018-02-23 21:58:32 +0000

    add CSV logging to gpudeets.py, tidy

--- gpudeets.py
+++ gpudeets.py
@@ -36,31 +36,48 @@ usage:
     program [options]
 
 options:
-    -h, --help          display help message
-    --version           display version and exit
-    --interval=INT      time between each loop (s) [default: 1]
-    --graphpower        power graph display
-    --graphtemperature  temperature graph display
-    --table             table display
-"""
+    -h, --help               display help message
+    --version                display version and exit
 
-name    = "gpudeets"
-version = "2017-06-02T0050Z"
+    --graphpower             power graph display
+    --graphtemperature       temperature graph display
+    --table                  table display
+
+    --CSV_logging=BOOL       log to CSV file            [default: false]
+    --filepath_CSV=FILEPATH  filepath for CSV logging   [default: gpudeets.csv]
+
+    --interval=INT           time between each loop (s) [default: 10]
+"""
 
 import datetime
 import docopt
+import logging
+import os
 import time
 
 import datavision
+import pandas as pd
 import pyprel
 import shijian
+import technicolor
+
+name    = "gpudeets"
+version = "2018-02-23T2158Z"
+
+log = logging.getLogger(name)
+log.addHandler(technicolor.ColorisingStreamHandler())
+log.setLevel(logging.INFO)
 
 def main(options):
 
+    graph_power       =     options["--graphpower"]
+    graph_temperature =     options["--graphtemperature"]
+    table             =     options["--table"]
+    CSV_logging       =     options["--CSV_logging"].lower() == "true"
+    filepath_CSV      =     options["--filepath_CSV"]
     interval          = int(options["--interval"])
-    graph_power       = options["--graphpower"]
-    graph_temperature = options["--graphtemperature"]
-    table             = options["--table"]
+
+    if CSV_logging: log.info("logging to CSV " + filepath_CSV)
 
     command_general     = "nvidia-smi "                      \
                               "--query-gpu="                 \
@@ -83,59 +100,70 @@ def main(options):
                               "--format=csv,noheader"
 
     measurements = []
-
     try:
-
         while True:
-
             if not graph_power and not graph_temperature:
-
-                timestamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%SZ")
-                result    = shijian.engage_command(command = command_general)
-                data      = [datum.strip() for datum in result.split(",")]
-
-                temperature  = str(data[1] + " °C")
-                power_draw   = str(data[2])
-                utilization  = str(data[3])
-                memory_used  = str(data[4])
-                memory_total = str(data[5])
-
+                timestamp          = datetime.datetime.utcnow()
+                timestamp_string   = timestamp.strftime("%Y-%m-%dT%H%M%SZ")
+                result             = shijian.engage_command(command = command_general)
+                data               = [datum.strip() for datum in result.split(",")]
+                temperature        = str(data[1])
+                temperature_string = temperature + " °C"
+                power_draw         = str(data[2])
+                utilization        = str(data[3])
+                memory_used        = str(data[4])
+                memory_total       = str(data[5])
                 if table:
-
                     print(pyprel.Table(
                     contents = [[
-                                   timestamp,
-                                   temperature,
+                                   timestamp_string,
+                                   temperature_string,
                                    utilization,
                                    memory_used,
                                    memory_total,
                                    power_draw
                                ]]
                     ))
-
+                if CSV_logging:
+                    df = pd.DataFrame(columns = [
+                        "datetime",
+                        "temperature_C",
+                        "power_draw_W",
+                        "utilization_MiB",
+                        "memory_used_MiB",
+                        "memory_total_percentage"
+                    ])
+                    df = df.append(
+                        {
+                            "datetime"               : timestamp,
+                            "temperature_C"          : temperature,
+                            "power_draw_W"           : power_draw[:-2],
+                            "utilization_MiB"        : utilization[:-4],
+                            "memory_used_MiB"        : memory_used[:-4],
+                            "memory_total_percentage": memory_total
+                        },
+                        ignore_index = True
+                    )
+                    log.info(timestamp_string + " log to CSV " + filepath_CSV)
+                    df.to_csv(filepath_CSV, header = not os.path.isfile(filepath_CSV), index = False, mode = "a")
                 else:
-
-                    temperature  = temperature.rjust(5)
-                    power_draw   = power_draw.rjust(8)
-                    utilization  = utilization.rjust(8)
-                    memory_used  = memory_used.rjust(8)
-                    memory_total = memory_total.rjust(5)
-
+                    temperature_string = temperature_string.rjust(5)
+                    power_draw         = power_draw.rjust(8)
+                    utilization        = utilization.rjust(8)
+                    memory_used        = memory_used.rjust(8)
+                    memory_total       = memory_total.rjust(5)
                     print(
-                        "|{timestamp}|{temperature}|{power_draw}"\
+                        "|{timestamp_string}|{temperature_string}|{power_draw}"\
                         "|{utilization}|{memory_used}|{memory_total}|".format(
-                        timestamp    = timestamp,
-                        temperature  = temperature,
-                        power_draw   = power_draw,
-                        utilization  = utilization,
-                        memory_used  = memory_used,
-                        memory_total = memory_total
+                        timestamp_string   = timestamp_string,
+                        temperature_string = temperature_string,
+                        power_draw         = power_draw,
+                        utilization        = utilization,
+                        memory_used        = memory_used,
+                        memory_total       = memory_total
                     ))
-
                 time.sleep(interval)
-
             elif graph_power or graph_temperature:
-
                 if graph_power:
                     result = shijian.engage_command(command = command_power)
                     result = result.strip().strip(" W")
@@ -143,18 +171,14 @@ def main(options):
                     result = shijian.engage_command(command = command_temperature)
                 measurements.append(float(result.strip()))
                 measurements = measurements[-20:]
-
                 y = measurements
                 x = range(0, len(y))
                 plot = datavision.TTYFigure()
                 tmp = plot.plot(x, y, marker = "_o")
                 print(tmp)
-
                 time.sleep(interval)
                 print(chr(27) + "[2J")
-
     except KeyboardInterrupt:
-
         print("")
 
 if __name__ == "__main__":
